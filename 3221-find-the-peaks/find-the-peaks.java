2a02f4284f2f9dbc1d5d0f0036944445aeb5e0ed
Vijay Sambari <76894452+vijaysambari@users.noreply.github.com>
2025-04-29 19:43:41 -0400

    Time: 1 ms (100.00%) | Memory: 44.7 MB (20.76%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+
+        List<Integer> peak = new ArrayList<>();
+
+        for (int i = 1; i<mountain.length-1;i++){
+            if((mountain[i]>mountain[i+1]) && (mountain[i]>mountain[i-1])){
+                peak.add(i);
+            }
+        }
+
+
+        return peak;
+        
+    }
+}
